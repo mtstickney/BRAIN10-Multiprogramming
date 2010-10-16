dd7e47493f85ce273b22493a8687f6fb0fdfcbc8
Matthew Stickney <fiendishlinux@gmail.com>
2010-10-15 21:34:35 -0400

    Add virtual addressing to memory module

--- mem.c
+++ mem.c
@@ -2,24 +2,26 @@
 #include <string.h>
 #include "mem.h"
 
-static char mem[4*100];
+#define PROCS 10
+
+static char mem[4*100*PROCS];
 
 void set_mem(char a) {
-	memset(mem, a, 4*100);
+	memset(mem, a, 4*100*PROCS);
 }
 
-static char *get_memp(unsigned int addr) {
+static char *get_memp(unsigned int pid, unsigned int addr) {
 	if (addr > 99) {
 		return NULL;
 	}
-	return &mem[4*addr];
+	return &mem[pid*100+4*addr];
 }
 
 /* Note: load and store convert endianness of words. */
-int load(unsigned int addr, char *dest) {
+int load(unsigned int pid, unsigned int addr, char *dest) {
 	void *src;
 
-	src = get_memp(addr);
+	src = get_memp(pid, addr);
 	if (src == NULL) {
 		fprintf(stderr, "load: invalid address %d\n", addr);
 		return -1;
@@ -28,11 +30,11 @@ int load(unsigned int addr, char *dest) {
 	return 0;
 }
 
-int store(const char *src, unsigned int addr)
+int store(unsigned int pid, const char *src, unsigned int addr)
 {
 	char *dest;
 
-	dest = get_memp(addr);
+	dest = get_memp(pid, addr);
 	if (dest == NULL) {
 		fprintf(stderr, "store: invalid address %d\n", addr);
 		return -1;
@@ -44,16 +46,18 @@ int store(const char *src, unsigned int addr)
 
 void print_mem()
 {
-	int i,j;
+	int i,j,k;
 	char *p;
-
-	for (i=0; i<100; i++) {
-		printf("%02d ", i);
-		for (j=0; j<9; j++, i++) {
-			p = get_memp(i);
-			printf("%c%c%c%c ", p[0], p[1], p[2], p[3]);
+	
+	for (k=0; k<10; k++) {
+		for (i=0; i<100; i++) {
+			printf("%02d ", i);
+			for (j=0; j<9; j++, i++) {
+				p = get_memp(k, i);
+				printf("%c%c%c%c ", p[0], p[1], p[2], p[3]);
+			}
+			p=get_memp(k, i);
+			printf("%c%c%c%c\n", p[0], p[1], p[2], p[3]);
 		}
-		p=get_memp(i);
-		printf("%c%c%c%c\n", p[0], p[1], p[2], p[3]);
 	}
 }	
--- mem.h
+++ mem.h
@@ -1,4 +1,4 @@
-int load(unsigned int addr, char *dest);
-int store(const char *src, unsigned int addr);
+int load(unsigned int pid, unsigned int addr, char *dest);
+int store(unsigned int pid, const char *src, unsigned int addr);
 void print_mem();
 void set_mem(char a);
